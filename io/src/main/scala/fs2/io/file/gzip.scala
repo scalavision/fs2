a16efd4d1cee960028a7a316c8af4e38dae74f48
Scalavision <scalavision@gmail.com>
2018-06-14 18:38:09 +0200

    wip - playing around with the fs2 Pull api, next is cleanup and extract header into GZipHeader case class

--- io/src/main/scala/fs2/io/file/gzip.scala
+++ io/src/main/scala/fs2/io/file/gzip.scala
@@ -4,19 +4,153 @@ package file
 
 import java.nio.file.{Path, StandardOpenOption}
 
-import cats.effect.{Sync}
+import cats.effect.Sync
 import fs2.Stream
 
 object gzip {
 
   // gzip magic
-  private final val ID1 = 0x1F
-  private final val ID2 = 0x8b
-  private val CM = 8
-  private val initialHeaderLength = 10
+  private final val ID1 = 0x1F.toByte
+  private final val ID2 = 0x8b.toByte
+  // gzip compression method, 8 is the only supported method
+  private final val CM = 0x08.toByte
+
+  // we take the number of bytes that gives the initial info about the header
+  private final val initialHeaderLength = 11
+
+  // Bit offsets for the flag
+  private final val FEXTRA = 4
+  private final val FNAME = 8
+  private final val FCOMMENT = 16
+  private final val FHCRC = 2
 
   val errorMessage: (Int, Int, String) => String = (flag, value, order) =>
     s"GZIP Magic $order byte not $flag, but $value, this may not be a GZIP filetype"
+
+  sealed trait GZipHeaderData
+  case class ModificationTime(epoch: Long) extends GZipHeaderData
+  case class ExtraField(value: Array[Byte]) extends GZipHeaderData
+  case class FileName(value: String) extends GZipHeaderData
+  case class Comment(value: String) extends GZipHeaderData
+  case class CRC16(value: Option[(Byte, Byte)]) extends GZipHeaderData
+  case class CRC32(value: (Byte, Byte, Byte, Byte)) extends GZipHeaderData
+  case class ISIZE(value: (Byte, Byte, Byte, Byte)) extends GZipHeaderData
+
+  case class GZipHeader(
+      modificationTime: ModificationTime,
+      extraField: ExtraField,
+      fileName: FileName,
+      comment: Comment,
+      CRC16: CRC16,
+      CRC32: CRC32,
+      ISIZE: ISIZE
+  )
+
+  //TODO: Skip the concept of returning a Pull[F, Byte, Unit]
+  // instead return an Either of Header or something ???
+  def headerValidator[F[_]](chunkSize: Int, offset: Long = 0L)(
+      h: FileHandle[F]): Pull[F, Byte, Unit] = {
+
+    val validHeader: Chunk.Bytes => Boolean = bytes =>
+      bytes.at(0) == ID1 && bytes.at(1) == ID2 && bytes.at(2) == CM
+
+    def skipOptionalField(bitIndex: Int): Byte => Int =
+      byte =>
+        if (byte.&(bitIndex) > 0) 1
+        else 0
+
+    def skipHeader(
+        nrOfOptionalTextFields: Int,
+        nrOfBytesInExtraFlags: Int,
+        totalOffset: Long
+    ): Pull[F, Byte, Unit] =
+      // We first skip through optional flag extra
+      if (nrOfBytesInExtraFlags > 0) {
+        Pull.eval(h.read(nrOfBytesInExtraFlags, totalOffset)) >>
+          Pull.outputChunk(Chunk.empty[Byte]) >> skipHeader(nrOfOptionalTextFields,
+                                                            0,
+                                                            totalOffset + nrOfBytesInExtraFlags)
+        // Then we skip through optional textfields, searching for String terminator
+      } else if (nrOfOptionalTextFields > 0) {
+        Pull.eval(h.read(1, totalOffset)).flatMap {
+          case Some(data) =>
+            if (data(0) == 0x0)
+              Pull.outputChunk(Chunk.empty[Byte]) >> skipHeader(nrOfOptionalTextFields - 1,
+                                                                0,
+                                                                totalOffset + 1)
+            else {
+
+              Pull.outputChunk(Chunk.empty[Byte]) >> skipHeader(nrOfOptionalTextFields,
+                                                                0,
+                                                                totalOffset + 1)
+            }
+          case None =>
+            Pull.done
+        }
+        // Finally we finish up with an empty chunk
+      } else
+        Pull.outputChunk(Chunk.empty[Byte])
+
+    def go(nextChunkSize: Int,
+           totalOffset: Long,
+           initialHeader: Boolean = true): Pull[F, Byte, Unit] =
+      Pull.eval(h.read(nextChunkSize, totalOffset)).flatMap {
+
+        case Some(data) =>
+          val bytes: Chunk.Bytes = data.toBytes
+
+          if (initialHeader) {
+            if (bytes.length == initialHeaderLength && validHeader(bytes)) {
+
+              val flags: Byte = bytes.at(4)
+              val extraFlag = skipOptionalField(FEXTRA)(flags)
+              //val hasCRC = skipOptionalField(FHCRC)(flags)
+
+              //var innerOffset = 0
+
+              val nrOfOptionalTextFields =
+                skipOptionalField(FNAME)(flags) + skipOptionalField(FCOMMENT)(flags)
+
+              println("textfields: " + nrOfOptionalTextFields)
+
+              if (extraFlag > 0) {
+                val extraBytes = bytes.at(9) + bytes.at(10)
+                println(
+                  "We have an extra flag of size: !" + extraBytes + " given byte the byte sequence: " + bytes
+                    .at(9) + " " + bytes.at(10))
+                skipHeader(nrOfOptionalTextFields, extraBytes, totalOffset)
+              } else {
+                skipHeader(nrOfOptionalTextFields, 0, totalOffset)
+              }
+
+            } else
+              Pull.raiseError(
+                new Throwable("Not a valid GZIP file, or compression method unsupported!"))
+          } else {
+            Pull.done
+          }
+
+        case None => Pull.done
+      }
+
+    go(initialHeaderLength, 0)
+
+  }
+
+  def unzip[F[_]: Sync](path: Path, chunkSize: Int): Stream[F, Byte] =
+    pulls
+      .fromPath(path, List(StandardOpenOption.READ))
+      .flatMap { c =>
+        headerValidator(chunkSize)(c.resource) //>>
+      //pulls.readAllFromFileHandle(chunkSize)(c.resource)
+      }
+      .stream
+
+}
+
+/*
+
+Old version kept for reference ...
 
   def extractHeaderInformation[F[_]](chunkSize: Int, offset: Long = 0L)(
       h: FileHandle[F]
@@ -33,7 +167,7 @@ object gzip {
 
     // skip zero terminated strings in header
     // return new offset
-    def skipZeroTerminatedStrings(offset: Long): Long = ???
+//    def skipZeroTerminatedStrings(offset: Long): Long = ???
 
     Pull.eval(h.read(initialHeaderLength, offset)).flatMap {
       case Some(initialHeader) =>
@@ -46,7 +180,7 @@ object gzip {
         } else if (initialHeader(2) != CM) {
           Pull.raiseError(new Throwable("GZIP Compression method is not supported"))
         } else {
-          val flags = initialHeader(3)
+          //val flags = initialHeader(3)
           val extraBytesFlag = initialHeader(8)
           // we count the number of flags with nullterminators
           // we extract the number of bytes we need to pull out
@@ -63,13 +197,4 @@ object gzip {
     }
   }
 
-  def unzip[F[_]: Sync](path: Path, chunkSize: Int): Stream[F, Byte] =
-    pulls
-      .fromPath(path, List(StandardOpenOption.READ))
-      .flatMap { c =>
-        extractHeaderInformation(chunkSize)(c.resource) >>
-          pulls.readAllFromFileHandle(chunkSize)(c.resource)
-      }
-      .stream
-
-}
+ */
--- io/src/test/scala/fs2/io/file/gzipSpec.scala
+++ io/src/test/scala/fs2/io/file/gzipSpec.scala
@@ -2,15 +2,18 @@ package fs2
 package io
 package file
 
+import java.nio.file.Paths
+
 //import scala.concurrent.duration._
-//
-//import cats.effect.IO
+
+import cats.effect.IO
 //import cats.implicits._
 //import java.io.IOException
-//import java.nio.file._
+import java.nio.file._
 //import java.nio.file.attribute.BasicFileAttributes
-//
-//import TestUtil._
+import fs2._
+//import fs2.internal.ThreadFactories
+import TestUtil._
 
 class gzipSpec extends Fs2Spec {
   //      runLog {
@@ -20,6 +23,21 @@ class gzipSpec extends Fs2Spec {
 
   "gzip" - {
     "supports extracting a file" in {
+
+//      val simpleHeader = immutable
+//        .Seq[Int](
+//          0x1f, 0x8b, 0x08, 0x08, 0x1e, 0x28, 0xa2, 0x52, 0x00, 0x03, 0x68, 0x75, 0x6d, 0x61, 0x6e,
+//          0x5f
+//        )
+//        .map(_.toByte)
+
+      runLog {
+        gzip.unzip[IO](
+          Paths.get(
+            "/home/obiwan/stash/sources/scala/tools/cmdbuilder/OldProject/PipeGen/src/main/resources/zipTest.fai.gz"),
+          100)
+      }
+
       1 shouldBe (1)
     }
   }
